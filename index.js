d1e7f8c6c64e440657bc7b974f96adf25834cb25
unknown <you@example.com>
2024-02-02 11:54:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const balances = ['$123.10', '$61 123.23', '$1,321,123.10', '$0.00', '$799.50', '$1 916 161 123.25'];
+
+const balanceNums = balances.map(balance => parseFloat(balance.replace(/[^\d.]/g, '')));
+console.log(balanceNums);
